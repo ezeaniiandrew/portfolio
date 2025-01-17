9e772b99b6a118facc1a690d4bdafcdf8c4452da
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-01-17 20:30:03 +0100

    add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,14 @@ export default {
     extend: {
       backgroundImage: {
         "not-found-bg": "url('./src/assets/not-found-bg.webp')",
+        "noise-bg": "url('./src/assets/noise.webp')",
       },
       colors: {
         "brand-secondary": "#FEB273",
+        "brand-tertiary-light": "#BDD9D8",
         "paragraph-tertiary": "#AAB0B1",
         "paragraph-black": "#03141A",
+        "nav-bg-black": "#0C0E0E",
       },
     },
   },
