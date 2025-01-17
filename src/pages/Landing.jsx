6db4ed9abdd7cc89d20372efe2f2047af94ec294
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-01-17 20:29:25 +0100

    create navbar

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,10 @@
+import NavBar from "../components/NavBar";
+
 function Landing() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="dark:text-brand-tertiary-light col-start-1 -col-end-1 bg-not-found-bg h-screen bg-cover bg-center grid grid-cols-12 relative">
+      <NavBar />
+    </section>
   );
 }
 
