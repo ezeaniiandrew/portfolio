4e485a3238d4a645fb92efa1ece8385124dc8119
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-01-22 14:35:17 +0100

    update 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import diagonalArrow from "@/assets/arrow.svg";
 
 function NotFound() {
@@ -8,14 +9,17 @@ function NotFound() {
         <p className="text-8xl ml-20">Error</p>
       </div>
       <p>The page you are looking for does not exist.</p>
-      <button className="border-solid border border-brand-secondary rounded-[100px] flex items-center gap-3 font-medium pr-3">
-        <span className="px-[20px] py-[12px] rounded-tr-[150px]  rounded-tl-[100px] rounded-bl-[100px] bg-brand-secondary border border-brand-secondary border-solid text-paragraph-black">
-          Back to home
+      <Link
+        to="/"
+        className="border-solid border border-brand-secondary rounded-[100px] flex items-center gap-3 font-medium pr-3"
+      >
+        <span className="px-[20px] py-[12px] rounded-tr-[150px] rounded-tl-[100px] rounded-bl-[100px] bg-brand-secondary border border-brand-secondary border-solid text-paragraph-black m-w-[250px]">
+          Go back home
         </span>
         <span>
           <img src={diagonalArrow} alt="" />
         </span>
-      </button>
+      </Link>
     </div>
   );
 }
