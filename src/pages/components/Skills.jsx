c7785eec6834b69015aefee3e1228aa794d9acc1
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-02-25 17:30:53 +0100

    change section heading

--- src/pages/components/Skills.jsx
+++ src/pages/components/Skills.jsx
@@ -29,7 +29,7 @@ function Skills() {
   return (
     <section className="col-start-1 md:col-start-2 -col-end-1 md:col-end-12 dark:text-brand-tertiary-light flex flex-col gap-10 items-center justify-center">
       <h2 className="text-3xl md:text-[3.375rem] font-semibold mb-4 capitalize text-brand-secondary">
-        My expertise
+        My skills
       </h2>
       <div className="flex flex-col md:flex-row w-full md:w-auto px-5 md:px-0 md:flex-wrap gap-4 md:justify-center">
         {skillSets.map((skill, index) => (
