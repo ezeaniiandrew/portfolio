af9ada1826e2b8742271d43e573ad900fc485d2e
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-03-23 09:53:06 +0100

    updates

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,12 +7,12 @@ function Button({ className, text, children }) {
       className={`border-solid border border-brand-secondary rounded-[100px] flex items-center gap-3 font-medium pr-3 ${className}`}
     >
       {text && (
-        <span className="md:px-[20px] md:py-[12px] rounded-tr-[150px] rounded-tl-[100px] rounded-bl-[100px] bg-brand-secondary border border-brand-secondary border-solid text-paragraph-black">
+        <span className="md:px-[20px] grid place-content-center md:py-[12px] h-full basis-10/12 rounded-tr-[150px] rounded-tl-[100px] rounded-bl-[100px] bg-brand-secondary border border-brand-secondary border-solid text-paragraph-black">
           {text}
         </span>
       )}
       {children && children}
-      <span>
+      <span className="basis-[fit-content]">
         <img src={diagonalArrow} alt="" />
       </span>
     </button>
