bc8e46d926eab494ccd6f48decdf6c1185576e4a
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-04-11 22:40:47 +0100

    remove bubble component

--- src/pages/components/index.js
+++ src/pages/components/index.js
@@ -3,5 +3,4 @@ export { default as AboutMe } from "./AboutMe";
 export { default as Skills } from "./Skills";
 export { default as ExploreProjects } from "./ExploreProjects";
 export { default as ArticlesSection } from "./ArticlesSection";
-export { default as BubblesContainer } from "./BubblesContainer";
 export { default as ProjectDetail } from "./ProjectDetail";
