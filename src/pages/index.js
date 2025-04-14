be78e0779d00b0bfc5dad0bba10407ca6b66c687
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-04-14 15:53:26 +0100

    add contact page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Landing } from "./Landing";
 export { default as NotFound } from "./NotFound";
 export { default as Projects } from "./projects";
+export { default as Contact } from "./Contact";
