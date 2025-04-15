8b7f3dfbd02740c2b3cbebe873efcac325d598c9
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-04-15 08:30:21 +0100

    refactor: move skillset to data folder

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,16 @@
 import { FaBehance, FaLinkedin } from "react-icons/fa";
 import { SiLinktree } from "react-icons/si";
 import { VscGithub } from "react-icons/vsc";
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaSass } from "react-icons/fa";
+import {
+  SiTypescript,
+  SiNextdotjs,
+  SiFirebase,
+  SiTestinglibrary,
+  SiTailwindcss,
+  SiFramer,
+  SiVitest,
+} from "react-icons/si";
 
 export const socialLinks = {
   developer: [
@@ -12,3 +22,18 @@ export const socialLinks = {
     { link: "www.linkedin.com/in/ezeani-andrew", icon: FaLinkedin },
   ],
 };
+
+export const skillSets = [
+  { icon: FaHtml5, color: "#E34F26", name: "HTML5" },
+  { icon: FaCss3Alt, color: "#1572B6", name: "CSS3" },
+  { icon: FaSass, color: "#CC6699", name: "Sass" },
+  { icon: FaJs, color: "#F7DF1E", name: "JavaScript" },
+  { icon: FaReact, color: "#61DAFB", name: "React" },
+  { icon: SiVitest, color: "#6E9F18", name: "Vitest" },
+  { icon: SiNextdotjs, color: "#FFFFFF", name: "Next.js" },
+  { icon: SiFirebase, color: "#FFCA28", name: "Firebase" },
+  { icon: SiTypescript, color: "#3178C6", name: "TypeScript" },
+  { icon: SiTestinglibrary, color: "#E33332", name: "React Testing Library" },
+  { icon: SiFramer, color: "#0055FF", name: "Framer Motion" },
+  { icon: SiTailwindcss, color: "#38B2AC", name: "Tailwind CSS" },
+];
--- src/pages/components/Skills.jsx
+++ src/pages/components/Skills.jsx
@@ -1,29 +1,4 @@
-import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaSass } from "react-icons/fa";
-
-import {
-  SiTypescript,
-  SiNextdotjs,
-  SiFirebase,
-  SiTestinglibrary,
-  SiTailwindcss,
-  SiFramer,
-  SiVitest,
-} from "react-icons/si";
-
-const skillSets = [
-  { icon: <FaHtml5 color="#E34F26" />, name: "HTML5" },
-  { icon: <FaCss3Alt color="#1572B6" />, name: "CSS3" },
-  { icon: <FaSass color="#CC6699" />, name: "Sass" },
-  { icon: <FaJs color="#F7DF1E" />, name: "JavaScript" },
-  { icon: <FaReact color="#61DAFB" />, name: "React" },
-  { icon: <SiVitest color="#6E9F18" />, name: "Vitest" },
-  { icon: <SiNextdotjs color="#FFFFFF" />, name: "Next.js" },
-  { icon: <SiFirebase color="#FFCA28" />, name: "Firebase" },
-  { icon: <SiTypescript color="#3178C6" />, name: "TypeScript" },
-  { icon: <SiTestinglibrary color="#E33332" />, name: "React Testing Library" },
-  { icon: <SiFramer color="#0055FF" />, name: "Framer Motion" },
-  { icon: <SiTailwindcss color="#38B2AC" />, name: "Tailwind CSS" },
-];
+import { skillSets } from "../../data";
 
 function Skills() {
   return (
@@ -37,7 +12,9 @@ function Skills() {
             key={index}
             className="flex items-center p-4 rounded-lg border border-tertiary-light"
           >
-            <div className="mr-2">{skill.icon}</div>
+            <div className="mr-2">
+              <skill.icon color={skill.color} />
+            </div>
             <p>{skill.name}</p>
           </div>
         ))}
