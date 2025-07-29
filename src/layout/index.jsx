38a9f47f3f36dde0b3a6c2a65806f5a0ddeeadfd
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-07-29 03:43:18 +0100

    fix: restore scroll position to the top when navigating between pages

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import { ScrollRestoration } from "react-router-dom";
 
 function Layout() {
   return (
@@ -10,6 +11,7 @@ function Layout() {
         <Outlet />
         <Footer />
       </>
+      <ScrollRestoration />
     </main>
   );
 }
