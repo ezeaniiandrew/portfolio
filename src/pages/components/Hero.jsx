ac17ecc792aa31e6c8fde8aba2abbb4a757051bb
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-08-16 13:34:49 +0100

    feat: add optional CTA to Hero component

--- src/pages/components/Hero.jsx
+++ src/pages/components/Hero.jsx
@@ -1,6 +1,12 @@
-function Hero({ headline, subText, highlightedText }) {
+import { Button } from "../../components";
+
+function Hero({ headline, subText, highlightedText, showCta }) {
   return (
-    <section className="px-5 bg-hero-image grid place-content-center text-brand-tertiary-light col-start-1 -col-end-1 bg-[#111111] h-screen bg-cover bg-no-repeat md:bg-center">
+    <section
+      className={`${
+        showCta && "pt-12"
+      } px-5 gap-6 bg-hero-image grid place-content-center text-brand-tertiary-light col-start-1 -col-end-1 bg-[#111111] h-screen bg-cover bg-no-repeat md:bg-center`}
+    >
       <h1 className="text-5xl text-center text-balance md:text-7xl font-bold text-white capitalize leading-tight">
         {headline} {""}
         {highlightedText && (
@@ -8,9 +14,17 @@ function Hero({ headline, subText, highlightedText }) {
         )}
       </h1>
       {subText && (
-        <p className="mt-6 text-2xl md:text-3xl text-center font-light max-w-3xl mx-auto text-balance">
+        <p className="text-2xl md:text-3xl text-center font-light max-w-3xl mx-auto text-balance">
           {subText}
         </p>
+      )}
+      {showCta && (
+        <Button
+          text="Get in touch"
+          className="capitalize mt-8 h-16 text-base md:max-w-80 md:mx-auto"
+          as={"link"}
+          href="mailto:ezeaniiandrew@example.com"
+        />
       )}
     </section>
   );
