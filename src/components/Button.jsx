b6dee4436b0d5e85f1b005c690bcb7a83d05284c
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-08-16 13:04:59 +0100

    feat: extend button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,26 @@
 /* eslint-disable react/prop-types */
 import diagonalArrow from "@/assets/arrow.svg";
 
-function Button({ className, text, children }) {
+function Button({ className, text, children, as, href }) {
+  if (as === "link") {
+    return (
+      <a
+        className={`border-solid border border-brand-secondary rounded-[100px] flex items-center gap-3 font-medium pr-3 ${className}`}
+        href={href}
+      >
+        {text && (
+          <span className="md:px-[20px] grid place-content-center md:py-[12px] h-full basis-10/12 rounded-tr-[150px] rounded-tl-[100px] rounded-bl-[100px] bg-brand-secondary border border-brand-secondary border-solid text-paragraph-black">
+            {text}
+          </span>
+        )}
+        {children && children}
+        <span className="basis-[fit-content]">
+          <img src={diagonalArrow} alt="" />
+        </span>
+      </a>
+    );
+  }
+
   return (
     <button
       className={`border-solid border border-brand-secondary rounded-[100px] flex items-center gap-3 font-medium pr-3 ${className}`}
