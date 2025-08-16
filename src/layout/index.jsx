229f840fc4c093c10babee773a3493cd7913dceb
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-08-16 13:37:50 +0100

    refactor: conditionally hide Footer on Contact page

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,14 +2,19 @@ import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import { ScrollRestoration } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function Layout() {
+  const location = useLocation();
+  // Hide footer on contact page
+  const hideFooter = location.pathname === "/contact";
+
   return (
     <main className="grid grid-cols-12 gap-y-20">
       <>
         <NavBar />
         <Outlet />
-        <Footer />
+        {!hideFooter && <Footer />}
       </>
       <ScrollRestoration />
     </main>
